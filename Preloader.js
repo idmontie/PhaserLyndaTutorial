5adfe3c40e4f2cd5efe31eda9af315265bfcb442
Ivan Montiel <idmontie@asu.edu>
2014-08-18 20:32:15 -0700

    Generating bunnies. Lesson 8

--- Preloader.js
+++ Preloader.js
@@ -19,6 +19,8 @@ BunnyDefender.Preloader.prototype.preload = function () {
 
   this.load.image('hill', 'images/hill.png');
   this.load.image('sky', 'images/sky.png');
+
+  this.load.atlasXML('bunny', 'images/spritesheets/bunny.png', 'images/spritesheets/bunny.xml');
 };
 
 BunnyDefender.Preloader.prototype.create = function () {
