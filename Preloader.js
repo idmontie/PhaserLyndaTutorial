9a87216beee2ef762898496af2524b5c6df82758
Ivan Montiel <idmontie@asu.edu>
2014-08-18 19:44:18 -0700

    Add Preloader.  Lesson 4.

---
+++ Preloader.js
@@ -0,0 +1,24 @@
+// Preloader.js
+
+BunnyDefender.Preloader = function (game) {
+  this.preloadBar = null;
+  this.titleText = null;
+  this.ready = false;
+};
+
+BunnyDefender.Preloader.prototype.preload = function () {
+  this.preloadBar = this.add.sprite(this.world.cenerX, this.world.centerY, 'preloaderBar');
+  this.preloadBar.anchor.setTo(0.5, 0.5);
+  this.load.setPreloadSprite(this.preloadBar);
+
+  this.titleText = this.add.image(this.world.centerX, this.world.centerY - 220, 'titleimage');
+  this.titleText.anchor.setTo(0.5, 0.5);
+};
+
+BunnyDefender.Preloader.prototype.create = function () {
+  this.preloadBar.cropEnabled = false;
+};
+
+BunnyDefender.Preloader.prototype.update = function () {
+  this.ready = true;
+};
